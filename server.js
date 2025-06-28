0a057865504f6a4dc43c88bc12bc955a9dbb3fd5
Pranjaljain2007 <pranjaljain0120@gmail.com>
2025-06-28 18:35:52 +0530

    changes

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ const userRoutes = require("./routes/userRoutes");
 connectDB();
 
 // Middleware
-app.use(cors({ origin: ['http://localhost:5173', '*'], credentials: true }));
+app.use(cors({
+  origin: 'https://rmww-frontend.onrender.com',
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Serve static images from /data
